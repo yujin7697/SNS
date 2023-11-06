2f2a185fb93dc597584d19b8283953e3d4b5097b
unknown <juj7697@gmail.com>
2023-11-06 15:34:33 +0900

    board crud 하는중..

--- src/main/java/com/example/demo/domain/dto/UserDto.java
+++ src/main/java/com/example/demo/domain/dto/UserDto.java
@@ -1,4 +1,41 @@
 package com.example.demo.domain.dto;
 
+import com.example.demo.domain.entity.User;
+import lombok.Data;
+
+@Data
 public class UserDto {
+
+    private String email;
+    private String password;
+
+    private String nickname;
+    private String name;
+    private String phone;
+    private String birth;
+    private String zipcode;
+    private String addr1;
+    private String addr2;
+    private String role;
+    private String profile;
+
+    private String provider;
+    private String providerId;
+
+    public static User dtoToEntity(UserDto dto){
+        User user = User.builder()
+                .email(dto.getEmail())
+                .password(dto.getPassword())
+                .nickname(dto.getNickname())
+                .name(dto.getName())
+                .phone(dto.getPhone())
+                .birth(dto.getBirth())
+                .zipcode(dto.getZipcode())
+                .addr1(dto.getAddr1())
+                .addr2(dto.getAddr2())
+                .role(dto.getRole())
+                .profile(dto.getProfile())
+                .build();
+        return user;
+    }
 }
--- src/main/java/com/example/demo/domain/entity/Board.java
+++ src/main/java/com/example/demo/domain/entity/Board.java
@@ -1,4 +1,33 @@
 package com.example.demo.domain.entity;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.format.annotation.DateTimeFormat;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Builder
 public class Board {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long number;
+    private String email;
+    private String nickname;
+    private String contents;
+
+    @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+    private LocalDateTime date;
+    private Long hits;
+    private Long like_count;
+
+    @ElementCollection
+    private List<String> files;
 }
